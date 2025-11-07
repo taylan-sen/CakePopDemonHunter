8aba4074b417e8b9dacd61221b9999b056fd2a65
Taylan "Ty" Sen <89081799+taylan-sen@users.noreply.github.com>
2025-11-07 10:54:54 -0500

    Added the opencv recommended video capture and display loop

--- mirror.py
+++ mirror.py
@@ -1,2 +1,37 @@
 # This file will be minimal opencv file for mirroring the webcam
+
 print('This is mirror.py')
+
+import numpy as np
+import cv2 as cv
+ 
+cap = cv.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    
+    # turn the image into mirror image
+    frame = cv.flip(frame, 1)
+
+    # if frame is read correctly ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+
+    # Our operations on the frame come here
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    # Display the resulting frame
+    #cv.imshow('frame', gray)
+    cv.imshow('frame', frame)
+    if cv.waitKey(1) == ord('q'):
+        break
+ 
+# When everything done, release the capture
+cap.release()
+cv.destroyAllWindows()
+
